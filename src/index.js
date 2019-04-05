adab18238ea6788abd74762677d47e233e37f94d
wj.overink <Willem-Jan.Overink@syskron.com>
2019-04-05 11:59:15 +0200

    delete keys

--- src/index.js
+++ src/index.js
@@ -12,14 +12,7 @@ import rootReducer from "./redux/reducers";
 import * as serviceWorker from "./serviceWorker";
 
 const store = createStore(rootReducer);
-const config = {
-  apiKey: "AIzaSyDfpuy73hTULR7DCZijiQLuyM5iVqAE6SM",
-  authDomain: "rsurance-web.firebaseapp.com",
-  databaseURL: "https://rsurance-web.firebaseio.com",
-  projectId: "rsurance-web",
-  storageBucket: "rsurance-web.appspot.com",
-  messagingSenderId: "965473148228"
-};
+const config = {};
 
 ReactDOM.render(
   <Provider store={store}>
